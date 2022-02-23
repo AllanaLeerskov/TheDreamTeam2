c50178cab561545fb5e2798bba55511e4f4b3557
Elektra <allanaleerskov@gmail.com>
2022-02-22 18:45:42 -0700

    clean up sql and table pages

--- db/schema.sql
+++ db/schema.sql
@@ -2,27 +2,26 @@ DROP DATABASE IF EXISTS random_shit;
 CREATE DATABASE random_shit;
 USE random_shit; 
 
--- CREATE TABLE products (
---   product_id INTEGER AUTO_INCREMENT PRIMARY KEY,
---   title VARCHAR(50) NOT NULL,
---   description TEXT,
---  price_id INTEGER,
---  photo INTEGER,
---  user_id INTEGER
--- );
+CREATE TABLE products (
+  product_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  description TEXT,
+ price_id INTEGER,
+ photo INTEGER,
+ user_id INTEGER
+);
 
--- CREATE TABLE price (
---   id INTEGER AUTO_INCREMENT PRIMARY KEY,
---   lower DECIMAL NOT NULL,
---   upper DECIMAL
+CREATE TABLE price (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  lower DECIMAL NOT NULL,
+  upper DECIMAL
+);
 
--- );
-
--- CREATE TABLE user (
---   id INTEGER AUTO_INCREMENT PRIMARY KEY,
---   name VARCHAR(50) NOT NULL,
---   email TEXT
---   password TEXT
--- );
+CREATE TABLE user (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  email TEXT
+  password TEXT
+);
 
 
